a8776df3f0367fe27f933b7ab900a2b296d18321
aleksandar <alpe88.dev@gmail.com>
2023-10-05 18:01:07 -0700

    restructure

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,18 @@
-export { Checkbox } from "./Checkbox";
-export { Button } from "./Button";
-export { ToggleButton } from "./ToggleButton";
+export { Checkbox } from "./components/Checkbox";
+export { Button } from "./components/Button";
+export { ToggleButton } from "./components/ToggleButton";
 
-export { List } from "./List/List";
-export { ListItem } from "./List/ListItem";
-export { ListCheckbox } from "./List/ListCheckbox";
+export { List } from "./components/List/List";
+export { ListItem } from "./components/List/ListItem";
+export { ListCheckbox } from "./components/List/ListCheckbox";
 
-export { Tag } from "./Tag/Tag";
-export { TagGroup } from "./Tag/TagGroup";
+export { Tag } from "./components/Tag/Tag";
+export { TagGroup } from "./components/Tag/TagGroup";
 
-export { Heading1 } from "./Typography/Heading1";
-export { Heading2 } from "./Typography/Heading2";
-export { Heading3 } from "./Typography/Heading3";
-export { Heading4 } from "./Typography/Heading4";
-export { Heading5 } from "./Typography/Heading5";
-export { Heading6 } from "./Typography/Heading6";
-export { Paragraph } from "./Typography/Paragraph";
+export { Heading1 } from "./components/Typography/Heading1";
+export { Heading2 } from "./components/Typography/Heading2";
+export { Heading3 } from "./components/Typography/Heading3";
+export { Heading4 } from "./components/Typography/Heading4";
+export { Heading5 } from "./components/Typography/Heading5";
+export { Heading6 } from "./components/Typography/Heading6";
+export { Paragraph } from "./components/Typography/Paragraph";
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,18 @@
 //vite.config.js
+
 import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
     lib: {
-      entry: "src/index.js", // Adjust the path to your library's entry point
-      name: "@alpe88/adar",
+      entry: resolve(__dirname, "lib/index.jsx"),
+      name: "adar",
+      fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
       // External dependencies
       external: ["react", "react-dom"],
       output: {
-        // Format for the generated bundle
         format: "umd",
         globals: {
           react: "React",
