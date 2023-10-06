a230c807448b60b97802b2600a0d7582ea7614e2
aleksandar <alpe88.dev@gmail.com>
2023-10-06 09:19:33 -0700

    x marks the spot

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,8 @@ import { defineConfig } from "vite";
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, "src/index.jsx"),
+      entry: resolve(__dirname, "src/index.js"),
       name: "adar",
-      fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
       external: ["react", "react-dom"],
