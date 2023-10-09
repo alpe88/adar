b692492cffb922f5c8b1a363ff737d4c4f1af0c5
aleksandar <alpe88.dev@gmail.com>
2023-10-08 19:06:06 -0700

    restructure

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,35 @@
-//vite.config.js
-import { resolve } from "path";
 import { defineConfig } from "vite";
+import { extname, relative, resolve } from "path";
+import { fileURLToPath } from "node:url";
+import { glob } from "glob";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react(), dts({ include: ["lib"] })],
   build: {
+    copyPublicDir: false,
     lib: {
-      entry: resolve(__dirname, "src/index.js"),
-      name: "adar",
+      entry: resolve(__dirname, "lib/main.js"),
+      formats: ["es"],
     },
     rollupOptions: {
-      external: ["react", "react-dom"],
+      external: ["react", "react/jsx-runtime"],
+      input: Object.fromEntries(
+        // https://rollupjs.org/configuration-options/#input
+        glob.sync("lib/**/*.{js,jsx}").map((file) => [
+          // 1. The name of the entry point
+          // lib/nested/foo.js becomes nested/foo
+          relative("lib", file.slice(0, file.length - extname(file).length)),
+          // 2. The absolute path to the entry file
+          // lib/nested/foo.js becomes /project/lib/nested/foo.js
+          fileURLToPath(new URL(file, import.meta.url)),
+        ])
+      ),
       output: {
-        format: "umd",
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-        },
+        assetFileNames: "assets/[name][extname]",
+        entryFileNames: "[name].js",
       },
     },
   },
