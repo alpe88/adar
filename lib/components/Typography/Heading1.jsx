6f378f2e1ba65d92ae16c10130d6fe8466c87d96
aleksandar <alpe88.dev@gmail.com>
2023-10-11 09:47:53 -0700

    new version released: v0.0.1-alpha.1.6.4.7

--- lib/components/Typography/Heading1.jsx
+++ lib/components/Typography/Heading1.jsx
@@ -1,3 +1,5 @@
 export function Heading1({ className, children }) {
-  return <h1 className={className}>{children}</h1>;
+  return (
+    <h1 className={`text-3xl font-bold underline ${className}`}>{children}</h1>
+  );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./main.js", "./lib/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
